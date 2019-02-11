d12846dcaad10a8a2afbaac6bbd64566a029e143
kin-alibaba <46918932+kin-alibaba@users.noreply.github.com>
2019-02-11 10:59:44 +0800

    Update app1_deploy.tf

--- app1_deploy.tf
+++ app1_deploy.tf
@@ -13,7 +13,7 @@ variable "app1"{
 	max_replicas = 10
 	cpu_threshold = 80
 	#image_repo = "registry-intl.ap-southeast-1.aliyuncs.com/kin-test-acr/todo"
-	image_repo = "seyantszkin/todo"			#
+	image_repo = "seyantszkin/demo"			#
 	image_ver = "v1.0"
 	svc_port = 80
 	container_port = 8080
